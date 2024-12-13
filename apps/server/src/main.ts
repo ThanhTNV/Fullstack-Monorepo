6f13e9c1a1bc239d8cc9361334d92b6fae6e9a30
ThanhTNV <thanhtran190304@gmail.com>
2024-12-13 17:06:36 +0700

    Fullstack-Monorepo-Angular-Nest

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -6,15 +6,24 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Demo Nx Repo')
+    .setDescription('The API demo')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
   const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  const openAPIPrefix = 'swagger';
+  SwaggerModule.setup(openAPIPrefix, app, documentFactory);
   const port = process.env.PORT || 3000;
+  app.setGlobalPrefix(globalPrefix);
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}/${openAPIPrefix}`
   );
 }
 
